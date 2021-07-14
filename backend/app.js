79a84fb191fcf18462ffa81dfb82881d73abd437
P1antain <lifeg1ance@yandex.ru>
2021-07-14 16:27:13 +0300

    refactorin port

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const page404 = require('./routes/page404');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 80 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
