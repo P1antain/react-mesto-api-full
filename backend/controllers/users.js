e52619f6a5a8507b6ed7e1cd2a291e87192f7b12
P1antain <lifeg1ance@yandex.ru>
2021-07-15 17:35:58 +0300

    add README.md

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -103,7 +103,7 @@ module.exports.login = (req, res, next) => {
           }
           const token = jwt.sign(
             { _id: user._id },
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
             { expiresIn: '7d' },
           );
 
