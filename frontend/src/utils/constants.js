ded7f2f627f38f089ed514f0803216930e74c189
P1antain <lifeg1ance@yandex.ru>
2021-07-17 16:02:10 +0300

    ref

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 const apiSettings = {
     url: "https://p1antain.students.nomoredomains.work",
-    token: "Bearer ${jwt}",
+    token: "1f24f5226ebed996f78c1755a1481e49a07a7f4a63760cd53ea8bd2e20bd3526",
 };
 
 export { apiSettings };
