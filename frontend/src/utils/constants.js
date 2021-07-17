931a3b6eff029036c940ad29c2bf50a971cceb7c
P1antain <lifeg1ance@yandex.ru>
2021-07-17 18:57:29 +0300

    delete files

--- frontend/src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const apiSettings = {
-    url: 'https://api.p1antain.students.nomoredomains.club',
-    token: localStorage.getItem('token')
-};
-
-export { apiSettings };
-
-//http://p1antain.students.nomoredomains.work
-//api.p1antain.students.nomoredomains.club
