ea7ee53d948c57d01cef535a759b743346bb65e3
P1antain <lifeg1ance@yandex.ru>
2021-07-17 16:05:50 +0300

    ref

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 const apiSettings = {
-    url: "https://p1antain.students.nomoredomains.work",
+    url: "https://api.p1antain.students.nomoredomains.club",
     token: "1f24f5226ebed996f78c1755a1481e49a07a7f4a63760cd53ea8bd2e20bd3526",
 };
 
